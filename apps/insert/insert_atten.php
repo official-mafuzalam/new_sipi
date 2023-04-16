804bf93d161c15bf38210b0dea657495dfb8c83f
Mafuz Alam <101239380+official-mafuzalam@users.noreply.github.com>
2023-04-17 01:57:17 +0600

    17-04-2023

--- apps/insert/insert_atten.php
+++ apps/insert/insert_atten.php
@@ -6,13 +6,15 @@ $user_id = $_GET['user'];
 $att_date = $_GET['d'];
 $roll = $_GET['r'];
 $name = $_GET['n'];
+$semester = $_GET['sem'];
+$technology = $_GET['t'];
 $atten_status = $_GET['s'];
 $att_month = $_GET['m'];
 $att_year = $_GET['y'];
 $inserter_id = $_GET['i_u_id'];
 
-$sql = "INSERT INTO stu_atten (user_id, att_date, roll_no, stu_name, atten_status, att_month, att_year, inserter_id) 
-	VALUES('$user_id', '$att_date', '$roll', '$name', '$atten_status', '$att_month', '$att_year', '$inserter_id') ";
+$sql = "INSERT INTO stu_atten (user_id, att_date, technology, semester, roll_no, stu_name, atten_status, att_month, att_year, inserter_id) 
+	VALUES('$user_id', '$att_date', '$technology', '$semester', '$roll', '$name', '$atten_status', '$att_month', '$att_year', '$inserter_id') ";
 $result = mysqli_query($con, $sql);
 
 if ($result)
